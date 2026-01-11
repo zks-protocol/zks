8fdaa5a0c1a64d37f52a097d86f83695281016a3
cswasif <md.wasif.faisal@g.bracu.ac.bd>
2026-01-11 17:05:24 +0600

    chore: remove bindings and android from git (keep local)

--- android/gradlew.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Gradle wrapper - please install Gradle and run: gradle :zks-uniffi:assembleRelease
-echo.
-echo Alternatively, you can:
-echo 1. Install Android Studio which includes Gradle
-echo 2. Download Gradle from https://gradle.org/install/
-echo 3. Use the gradle command directly if already installed
